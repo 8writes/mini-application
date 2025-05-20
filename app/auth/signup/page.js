d6482c58ba1a9d1ed32935bf17a580761535164c
Chisom Ozoemena <ozimmanuel79@gmail.com>
2025-05-20 12:02:42 +0100

    Update page.js

--- app/auth/signup/page.js
+++ app/auth/signup/page.js
@@ -137,10 +137,10 @@ export default function SignupPage() {
 
           <button
             type="submit"
-            disabled={true}
+            disabled={isSubmitting}
             className="w-full py-3 bg-gray-600 cursor-pointer hover:bg-gray-800 transition duration-150 text-white font-semibold rounded-lg disabled:opacity-50"
           >
-            {isSubmitting ? "Unavailable" : "Unavailable"}
+            {isSubmitting ? "Signing up... " : "Sign Up"}
           </button>
         </form>
 
