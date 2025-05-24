fae4a804c9040084f65b32e53f6157a1a9c08047
Chisom Ozoemena <ozimmanuel79@gmail.com>
2025-05-24 22:09:28 +0100

    s

--- app/(publicPages)/auth/signup/page.js
+++ app/(publicPages)/auth/signup/page.js
@@ -160,8 +160,8 @@ export default function SignupPage() {
 
           <button
             type="submit"
-            disabled={true}
-            onClick={() => toast.info("Not Live, Check back later")}
+            disabled={isSubmitting}
+            
             className="w-full py-3 bg-gray-600 cursor-pointer hover:bg-gray-800 transition duration-150 text-white font-semibold rounded-lg disabled:opacity-50"
           >
             {isSubmitting ? "Signing up... " : "Sign Up"}
