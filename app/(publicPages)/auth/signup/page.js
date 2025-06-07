9369e589bfa3f156713852a7c0ca5cc4b4489f85
Chisom Ozoemena <ozimmanuel79@gmail.com>
2025-06-07 11:23:46 +0100

    Update page.js

--- app/(publicPages)/auth/signup/page.js
+++ app/(publicPages)/auth/signup/page.js
@@ -7,6 +7,7 @@ import Link from "next/link";
 import { HiEye, HiEyeOff } from "react-icons/hi";
 import { billzpaddi } from "@/lib/client";
 import Image from "next/image";
+import { Suspense } from "react";
 
 export default function SignupPage() {
   const router = useRouter();
@@ -56,7 +57,7 @@ export default function SignupPage() {
         if (error || !referrer) {
           toast.warning("Invalid referral code");
           setFormData((prev) => ({ ...prev, referral_code: "" }));
-          return
+          return;
         }
       }
 
@@ -100,135 +101,149 @@ export default function SignupPage() {
   };
 
   return (
-    <main className="min-h-[100dvh] bg-gradient-to-r from-gray-700 to-gray-800 flex items-center justify-center px-4">
-      <div className="w-full max-w-md bg-white p-6 rounded-2xl shadow-lg">
-        <div className="text-center mb-6">
-          <div className="text-3xl font-bold text-blue-900 mb-2 flex items-center gap-1 justify-center">
-            <Image
-              src="/billzpaddi-logo-icon.png"
-              alt="BillzPaddi Logo"
-              width={20}
-              height={20}
-            />{" "}
-            𝗕𝗶𝗹𝗹𝘇𝗣𝗮𝗱𝗱𝗶
+    <Suspense
+      fallback={
+        <div className="flex justify-center items-center h-64">
+          <div className="flex justify-center py-10">
+            <img
+              src="/icons/loader-white.svg"
+              alt="Loading..."
+              className="w-14 h-14"
+            />
           </div>
-          <p className="text-blue-800">Create An Account</p>
         </div>
+      }
+    >
+      <main className="min-h-[100dvh] bg-gradient-to-r from-gray-700 to-gray-800 flex items-center justify-center px-4">
+        <div className="w-full max-w-md bg-white p-6 rounded-2xl shadow-lg">
+          <div className="text-center mb-6">
+            <div className="text-3xl font-bold text-blue-900 mb-2 flex items-center gap-1 justify-center">
+              <Image
+                src="/billzpaddi-logo-icon.png"
+                alt="BillzPaddi Logo"
+                width={20}
+                height={20}
+              />{" "}
+              𝗕𝗶𝗹𝗹𝘇𝗣𝗮𝗱𝗱𝗶
+            </div>
+            <p className="text-blue-800">Create An Account</p>
+          </div>
 
-        <form onSubmit={handleSubmit} className="space-y-4 text-gray-800">
-          <div className="grid md:grid-cols-2 gap-4">
+          <form onSubmit={handleSubmit} className="space-y-4 text-gray-800">
+            <div className="grid md:grid-cols-2 gap-4">
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-800">
+                  First Name
+                </label>
+                <input
+                  type="text"
+                  required
+                  placeholder="First Name"
+                  disabled={isSubmitting}
+                  value={formData.first_name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, first_name: e.target.value })
+                  }
+                  className="w-full px-4 py-2 border border-gray-400 rounded-md outline-none"
+                />
+              </div>
+              <div>
+                <label className="block mb-1 text-sm font-medium text-gray-800">
+                  Last Name
+                </label>
+                <input
+                  type="text"
+                  required
+                  placeholder="Last Name"
+                  disabled={isSubmitting}
+                  value={formData.last_name}
+                  onChange={(e) =>
+                    setFormData({ ...formData, last_name: e.target.value })
+                  }
+                  className="w-full px-4 py-2 border border-gray-400 rounded-md outline-none"
+                />
+              </div>
+            </div>
+
             <div>
               <label className="block mb-1 text-sm font-medium text-gray-800">
-                First Name
+                Email Address
               </label>
               <input
-                type="text"
+                type="email"
                 required
-                placeholder="First Name"
+                placeholder="Email"
                 disabled={isSubmitting}
-                value={formData.first_name}
+                value={formData.email}
                 onChange={(e) =>
-                  setFormData({ ...formData, first_name: e.target.value })
+                  setFormData({ ...formData, email: e.target.value })
                 }
                 className="w-full px-4 py-2 border border-gray-400 rounded-md outline-none"
               />
             </div>
+
             <div>
               <label className="block mb-1 text-sm font-medium text-gray-800">
-                Last Name
+                Password
+              </label>
+              <div className="relative">
+                <input
+                  type={showPassword ? "text" : "password"}
+                  required
+                  disabled={isSubmitting}
+                  value={formData.password}
+                  onChange={(e) =>
+                    setFormData({ ...formData, password: e.target.value })
+                  }
+                  className="w-full px-4 py-2 border pr-10 text-gray-800 border-gray-400 rounded-lg outline-none disabled:opacity-50"
+                  placeholder="******"
+                />
+                <span
+                  className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
+                  onClick={() => setShowPassword(!showPassword)}
+                >
+                  {showPassword ? <HiEyeOff size={20} /> : <HiEye size={20} />}
+                </span>
+              </div>
+            </div>
+
+            <div>
+              <label className="block mb-1 text-sm font-medium text-gray-800">
+                Referral Code (Optional)
               </label>
               <input
                 type="text"
-                required
-                placeholder="Last Name"
+                placeholder="Enter referral code"
                 disabled={isSubmitting}
-                value={formData.last_name}
+                value={formData.referral_code}
                 onChange={(e) =>
-                  setFormData({ ...formData, last_name: e.target.value })
+                  setFormData({ ...formData, referral_code: e.target.value })
                 }
                 className="w-full px-4 py-2 border border-gray-400 rounded-md outline-none"
               />
             </div>
-          </div>
 
-          <div>
-            <label className="block mb-1 text-sm font-medium text-gray-800">
-              Email Address
-            </label>
-            <input
-              type="email"
-              required
-              placeholder="Email"
+            <button
+              type="submit"
               disabled={isSubmitting}
-              value={formData.email}
-              onChange={(e) =>
-                setFormData({ ...formData, email: e.target.value })
-              }
-              className="w-full px-4 py-2 border border-gray-400 rounded-md outline-none"
-            />
-          </div>
+              className="w-full py-3 bg-gray-600 cursor-pointer hover:bg-gray-800 transition duration-150 text-white font-semibold rounded-lg disabled:opacity-50"
+            >
+              {isSubmitting ? "Signing up... " : "Sign Up"}
+            </button>
+          </form>
 
-          <div>
-            <label className="block mb-1 text-sm font-medium text-gray-800">
-              Password
-            </label>
-            <div className="relative">
-              <input
-                type={showPassword ? "text" : "password"}
-                required
-                disabled={isSubmitting}
-                value={formData.password}
-                onChange={(e) =>
-                  setFormData({ ...formData, password: e.target.value })
-                }
-                className="w-full px-4 py-2 border pr-10 text-gray-800 border-gray-400 rounded-lg outline-none disabled:opacity-50"
-                placeholder="******"
-              />
-              <span
-                className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500 cursor-pointer"
-                onClick={() => setShowPassword(!showPassword)}
-              >
-                {showPassword ? <HiEyeOff size={20} /> : <HiEye size={20} />}
-              </span>
-            </div>
-          </div>
-
-          <div>
-            <label className="block mb-1 text-sm font-medium text-gray-800">
-              Referral Code (Optional)
-            </label>
-            <input
-              type="text"
-              placeholder="Enter referral code"
-              disabled={isSubmitting}
-              value={formData.referral_code}
-              onChange={(e) =>
-                setFormData({ ...formData, referral_code: e.target.value })
-              }
-              className="w-full px-4 py-2 border border-gray-400 rounded-md outline-none"
-            />
-          </div>
-
-          <button
-            type="submit"
-            disabled={isSubmitting}
-            className="w-full py-3 bg-gray-600 cursor-pointer hover:bg-gray-800 transition duration-150 text-white font-semibold rounded-lg disabled:opacity-50"
-          >
-            {isSubmitting ? "Signing up... " : "Sign Up"}
-          </button>
-        </form>
-
-        <p className="text-gray-800 text-sm text-center mt-4">
-          Already have an account{" "}
-          <span className="rotate-20 inline-block">?</span>{" "}
-          <Link
-            href="/auth/login"
-            className="text-gray-800 cursor-pointer hover:text-gray-700"
-          >
-            Login
-          </Link>
-        </p>
-      </div>
-    </main>
+          <p className="text-gray-800 text-sm text-center mt-4">
+            Already have an account{" "}
+            <span className="rotate-20 inline-block">?</span>{" "}
+            <Link
+              href="/auth/login"
+              className="text-gray-800 cursor-pointer hover:text-gray-700"
+            >
+              Login
+            </Link>
+          </p>
+        </div>
+      </main>
+    </Suspense>
   );
 }
