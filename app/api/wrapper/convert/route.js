71de38441f0994724fafb5b62bd4076bf275e1a9
Chisom Ozoemena <ozimmanuel79@gmail.com>
2025-06-10 23:10:53 +0100

    n

--- app/api/wrapper/convert/route.js
+++ app/api/wrapper/convert/route.js
@@ -17,10 +17,13 @@ export async function POST(request) {
   // API Key Authentication
   const apiKey = request.headers.get("authorization")?.split("Bearer ")[1];
   if (!apiKey || apiKey !== process.env.NEXT_PUBLIC_BILLZ_AUTH_KEY) {
-    return new Response(JSON.stringify({ message: "Fuck Off", ok: false }), {
-      status: 401,
-      headers: { "Content-Type": "application/json" },
-    });
+    return new Response(
+      JSON.stringify({ message: "Invalid request", ok: false }),
+      {
+        status: 403,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
 
   // Rate limiting by IP
