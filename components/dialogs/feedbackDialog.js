dddc933d2e6529b74daa25d4b60fae5ce1c740a7
Chisom Ozoemena <ozimmanuel79@gmail.com>
2025-06-17 17:38:19 +0100

    Update feedbackDialog.js

--- components/dialogs/feedbackDialog.js
+++ components/dialogs/feedbackDialog.js
@@ -108,7 +108,7 @@ export default function TransactionStatusModal({
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
-          className="absolute inset-0 bg-black/50 backdrop-blur-sm"
+          className="absolute inset-0 bg-black/80 backdrop-blur-sm"
         />
 
         {/* Modal with slide-up animation */}
